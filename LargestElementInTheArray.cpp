6d42123c0dee2e78dfba66b5f36955c1a0075e5e
bishwo99 <112264657+bishwo99@users.noreply.github.com>
2024-12-19 13:24:55 +0600

    LargestElementInTheArray.cpp

--- LargestElementInTheArray.cpp
+++ LargestElementInTheArray.cpp
@@ -1,19 +1,12 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main()
-{
-
-    int n;
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++) cin>> arr[i];
-    int largest = arr[0];
-    for(int i=0;i<n;i++)
-    {
-        if(arr[i]>largest)
-            largest = arr[i];
-
+class Solution {
+  public:
+    int largest(vector<int> &arr) {
+        int large = arr[0];
+        for(int i=0;i<arr.size();i++)
+        {
+            if(arr[i] >= large)
+            large = arr[i];
+        }
+        return large;
     }
-
-    cout<<largest<<"\n";
-}
+};
