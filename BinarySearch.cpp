8f298828d2b0b8d6a5a0d8818d81e8e4d1c0c12e
bishwo99 <112264657+bishwo99@users.noreply.github.com>
2024-12-20 17:14:58 +0600

    Add files via upload

---
+++ BinarySearch.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+    int k;
+    cin>>k;
+
+    int low = 0;
+    int high = n-1;
+
+    bool findd = false;
+    while(low<=high)
+    {
+        int mid = (low + high)/2;
+        if(arr[mid] == k)
+        {
+            cout<< mid;
+            findd = true;
+            break;
+        }
+        else if(arr[mid]<k)
+            low = mid + 1;
+        else
+            high = mid -1;
+
+    }
+    if(!findd) cout<<low;
+
+
+
+}
