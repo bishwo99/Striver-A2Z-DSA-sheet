d41e770a9677718946d1308b3146bd38da68fabb
bishwo99 <112264657+bishwo99@users.noreply.github.com>
2024-12-21 23:14:52 +0600

    268 Missing Number.cpp

--- 268 Missing Number.cpp
+++ 268 Missing Number.cpp
@@ -1,37 +1,15 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main()
-{
-    int n;
-    cin>>n;
-    vector<int>v(n);
-    for(int i=0;i<n;i++) cin>>v[i];
-    for(int i=0;i<n;i++)
-    {
-        sort(v.begin(),v.end());
-    }
-    int c=1;
-    int low = 0;
-    int high = n;
-    if( n != v[n])
-    {
-        for(int i=1; i<n; i++)
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int Sn = (n*(n+1))/2;
+        int sum=0;
+        for(int i=0;i<n;i++)
         {
-
-            if(v[i] == c)
-            {
-                c++;
-            }
-
-            else if(v[i]!=c)
-            {
-                cout<<c;
-                break;
-            }
+            sum = sum + nums[i];
         }
+        return (Sn-sum);
+
 
     }
-    else
-        cout<<n;
-
-}
+};
