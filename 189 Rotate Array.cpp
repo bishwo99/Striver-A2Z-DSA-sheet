63dad8103ca2ab1c3c0fff48f91e1a8d02a1f469
bishwo99 <112264657+bishwo99@users.noreply.github.com>
2024-12-22 12:31:30 +0600

    Add files via upload

---
+++ 189 Rotate Array.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+ void RotateArray(vector<int>&nums, int n, int k){
+   k = k % n;
+
+   int tmp[k];
+
+   for(int i = 0; i < k; i++)
+   {
+      tmp[i] = nums[i+(n-k)];
+   }
+
+    for( int i = n ; i>= k; i-- )
+    {
+      nums[i] = nums[i-k];
+    }
+    for( int i = 0; i < k; i++ )
+    {
+        nums[i] = tmp[i];
+    }
+
+ }
+
+
+int main()
+{
+     int n;
+     cin>>n;
+     vector<int> nums(n);
+     for(int i=0;i<n;i++) cin>> nums[i];
+     int k;
+     cin>>k;
+     RotateArray(nums,n,k);
+     for(int i=0;i<n;i++) cout<<nums[i]<<" ";
+}
